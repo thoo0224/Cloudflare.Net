92088e98e38488658c1c816bd7a36bde4acc48ea
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-05-15 17:45:44 +0200

    added empty cloudflare response model

--- src/Cloudflare.Net/Objects/CloudflareResponse.cs
+++ src/Cloudflare.Net/Objects/CloudflareResponse.cs
@@ -4,17 +4,25 @@ using System.Linq;
 
 namespace Cloudflare.Net.Objects
 {
-    [DebuggerDisplay("{" + nameof(DebuggerDisplay) + "}")]
-    public class CloudflareResponse<T>
+
+    public class CloudflareResponse
     {
 
-        public T Result { get; set; }
         public bool Success { get; set; }
         public List<CloudflareError> Errors { get; set; }
         public List<string> Messages { get; set; }
         public CloudflareResultInfo ResultInfo { get; set; }
 
+    }
+
+    [DebuggerDisplay("{" + nameof(DebuggerDisplay) + "}")]
+    public class CloudflareResponse<T> : CloudflareResponse
+    {
+
+        public T Result { get; set; }
+
         private object DebuggerDisplay => Success ? Result : $"Error: {Errors.FirstOrDefault()?.Message}" as object;
 
     }
+
 }
