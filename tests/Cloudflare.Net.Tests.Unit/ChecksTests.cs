df37f507ecd6c6476398675b44fe14c56cade0f2
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-05-15 17:03:35 +0200

    added check tests and xunit config

--- tests/Cloudflare.Net.Tests.Unit/ChecksTests.cs
+++ tests/Cloudflare.Net.Tests.Unit/ChecksTests.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cloudflare.Net.Exceptions;
+using Cloudflare.Net.Utils;
+
+using Xunit;
 
 namespace Cloudflare.Net.Tests.Unit
 {
-    class ChecksTests
+    public class ChecksTests
     {
+
+        /*
+         *         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void NotNull(object obj, string name, string message = null)
+        {
+            if (obj != null)
+            {
+                return;
+            }
+
+            var msg = message ?? "{0} cannot be null.";
+            throw new CloudflareException(string.Format(msg, name));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void StringLength(string str, string name, int length, string message = null)
+        {
+            if(str.Length <= length)
+            {
+                return;
+            }
+
+            var msg = message ?? "{0} cannot be longer than {1} characters.";
+            throw new CloudflareException(string.Format(msg, name, length));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Regex(string str, string name, Regex regex, string message = null)
+        {
+            var match = regex.IsMatch(str);
+            if (match)
+            {
+                return;
+            }
+
+            var msg = message ?? "{0} did not match the pattern.";
+            throw new CloudflareException(string.Format(msg, name));
+        }*/
+
+        [Theory]
+        [InlineData("https", "http(s?)")]
+        [InlineData("https://thoo.nl", "http(s?)://thoo.nl")]
+        public void Regex_ShouldWork(string str, string pattern)
+        {
+            Checks.Regex(str, nameof(str), new System.Text.RegularExpressions.Regex(pattern));
+        }
+
+        [Theory]
+        [InlineData("ftp", "http(s?)")]
+        [InlineData("https://thoo.com", "http(s?)://thoo.nl")]
+        public void Regex_ShouldFail(string str, string pattern)
+        {
+            Assert.Throws<CloudflareException>(() => Checks.Regex(str, nameof(str), new System.Text.RegularExpressions.Regex(pattern)));
+        }
+
     }
 }
