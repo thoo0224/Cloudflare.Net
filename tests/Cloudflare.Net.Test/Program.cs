5c4c46b9bc055c281cf9fd92dfd53413db5ff6d9
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-05-15 19:20:04 +0200

    added test project

---
+++ tests/Cloudflare.Net.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Cloudflare.Net.Test
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            var key = Environment.GetEnvironmentVariable("cloudflare-api-key");
+            var email = Environment.GetEnvironmentVariable("cloudflare-api-email");
+
+            
+        }
+
+    }
+}
